e901a7eb34a85463c4bf96698942d67a0307f946
alquimar <alquimars@gmail.com>
2018-06-07 20:07:15 -0300

    Litagem de Contratos, Projetos de Assentamento por Contrato e Diagnóticos(Lotes) por Projeto de Assentamento

--- rural_norte/core/urls.py
+++ rural_norte/core/urls.py
@@ -6,8 +6,9 @@ app_name = "core"
 urlpatterns = [
     path("", views.LoteListView.as_view(), name="lote-list"),
     path("contratos/", views.listar_contratos, name="listar_contratos"),
+    path("contratos/<int:contrato_id>/projetos_assentamento/", views.listar_projetos_por_contrato, name="listar_projetos_por_contrato"),
+    path("contratos/<int:contrato_id>/projetos_assentamento/<int:pa_id>/diagnosticos/", views.listar_diagnosticos_por_projeto_assentamento, name="listar_diagnosticos_por_projeto_assentamento"),
     path("tabela/", views.table_view, name="table_view"),
     path("tabela2/", views.Teste.as_view(), name="teste_view"),
-    # path('detalhes/(?P<pk>\d+)/$', LoteDetailView.as_view(), name='lote-detail'),
     path('<int:pk>/', views.LoteDetailView.as_view(), name='lote-detail'),
 ]
--- rural_norte/core/views.py
+++ rural_norte/core/views.py
@@ -28,6 +28,33 @@ def listar_contratos(request):
     return render(request, template_name, context)
 
 
+def listar_projetos_por_contrato(request, contrato_id):
+    atualizado_em = datetime.now()
+    contrato = models.Contrato.objects.get(pk=contrato_id)
+    projetos_assentamento = models.ProjetoAssentamento.objects.filter(contrato=contrato_id)
+    template_name = 'core/listar_projetos_assentamento_por_contrato.html'
+    context = {
+        'projetos_assentamento': projetos_assentamento,
+        'contrato': contrato,
+        'atualizado_em': atualizado_em
+    }
+    return render(request, template_name, context)
+
+
+def listar_diagnosticos_por_projeto_assentamento(request, contrato_id, pa_id):
+    atualizado_em = datetime.now()
+    projeto_assentamento = models.ProjetoAssentamento.objects.get(pk=pa_id)
+    diagnosticos = models.Lote.objects.filter(projeto_assentamento=pa_id)
+    template_name = 'core/listar_diagnosticos_por_projeto_assentamento.html'
+    context = {
+        'diagnosticos': diagnosticos,
+        'projeto_assentamento': projeto_assentamento,
+        'atualizado_em': atualizado_em
+
+    }
+    return render(request, template_name, context)
+
+
 def listar_projetos_assentamento_por_contrato(request, pk):
     atualizado_em = datetime.now()
     projetos_assentamento = models.ProjetoAssentamento.objects.filter(contrato=pk)
